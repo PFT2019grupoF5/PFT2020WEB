4d255b1506d0e85306422187738a7a53a868ad35
federico.montanez <federico.montanez@utec.edu.uy>
2020-12-05 15:28:36 -0300

    Edición UsuarioBean

--- src/com/bean/UsuariosBean.java
+++ src/com/bean/UsuariosBean.java
@@ -18,10 +18,10 @@ import com.exception.ServiciosException;
 import com.services.UsuarioBeanRemote;
 
 @SuppressWarnings("deprecation")
-@ManagedBean(name="usuario")	
+@ManagedBean(name = "usuario")
 @ViewScoped
 public class UsuariosBean {
-	
+
 	private Long id;
 	private String nombre;
 	private String apellido;
@@ -29,137 +29,149 @@ public class UsuariosBean {
 	private String contrasena;
 	private String correo;
 	private tipoPerfil tipoPerfil;
-	
-	
+
 	private List<SelectItem> availablePerfiles;
-	
+
 	private static tipoPerfil perfilLogeado;
-	
+
 	private String radioFindBy;
-	
+
 	private String filtroBusqueda;
-	
+
 	private List<Usuario> usuariosList;
-	
+
 	private Usuario selectedUsuario;
-	
-	private boolean confirmarBorrado=false;
-	
+
+	private boolean confirmarBorrado = false;
+
 	@EJB
 	private UsuarioBeanRemote usuariosEJBBean;
-	
-	public String add(){
-		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Éxito al Registrar: ", "Usuario ingresado exitosamente!");
+
+	public String add() {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Éxito al Registrar: ",
+				"Usuario ingresado exitosamente!");
 		String retPage = "altaUsuarioPage";
 		try {
-			if(!tipoPerfil.ADMINISTRADOR.equals(perfilLogeado)) {
-				message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Falta de Permisos: ", "Debe ser un Usuario ADMINISTRADOR para poder acceder");
-			}else if(nombre.isEmpty() || nombre.length()>50) {
-				message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Registrar: ", "Campo Nombre no puede ser vacío o mayor a 50 caracteres");
-			}else if(apellido.isEmpty() || apellido.length()>50) {
-				message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Registrar: ", "Campo Apellido no puede ser vacío o mayor a 50 caracteres");
-			}else if(tipoPerfil==null) {
-				message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Registrar: ", "Debe Seleccionar un tipo de Perfil");
-			}else if(contrasena.length()==0){
-				message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Registrar: ", "Campo Contraseña no puede ser vacío");
-			}else if(contrasena.length()<8 || contrasena.length()>16){
-				message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Registrar: ", "Campo Contraseña debe tener un largo entre 8 y 16 caracteres");
-			}else if(nomAcceso.isEmpty() || nomAcceso.length()>50){
-				message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Registrar: ", "Campo nomAcceso no puede ser vacío o mayor a 50 caracteres");
-			}else if(!correo.contains("@")){
-				message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Registrar: ", "Campo Correo debe ser del formato : nombre @ dominio");
-			}else {
-				if(get()==null) {
-					usuariosEJBBean.add(nombre, apellido, nomAcceso, DigestUtils.md5Hex(contrasena), correo, tipoPerfil );
-				}else {
-					message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Error al Registrar: ", "El nombre de usuario provisto ya existe");
+			if (!tipoPerfil.ADMINISTRADOR.equals(perfilLogeado)) {
+				message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Falta de Permisos: ",
+						"Debe ser un Usuario ADMINISTRADOR para poder acceder");
+			} else if (nombre.isEmpty() || nombre.length() > 50) {
+				message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Registrar: ",
+						"Campo Nombre no puede ser vacío o mayor a 50 caracteres");
+			} else if (apellido.isEmpty() || apellido.length() > 50) {
+				message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Registrar: ",
+						"Campo Apellido no puede ser vacío o mayor a 50 caracteres");
+			} else if (tipoPerfil == null) {
+				message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Registrar: ",
+						"Debe Seleccionar un tipo de Perfil");
+			} else if (contrasena.length() == 0) {
+				message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Registrar: ",
+						"Campo Contraseña no puede ser vacío");
+			} else if (contrasena.length() < 8 || contrasena.length() > 16) {
+				message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Registrar: ",
+						"Campo Contraseña debe tener un largo entre 8 y 16 caracteres");
+			} else if (nomAcceso.isEmpty() || nomAcceso.length() > 50) {
+				message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Registrar: ",
+						"Campo nomAcceso no puede ser vacío o mayor a 50 caracteres");
+			} else if (!correo.contains("@")) {
+				message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error al Registrar: ",
+						"Campo Correo debe ser del formato : nombre @ dominio");
+			} else {
+				if (get() == null) {
+					usuariosEJBBean.add(nombre, apellido, nomAcceso, DigestUtils.md5Hex(contrasena), correo,
+							tipoPerfil);
+				} else {
+					message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Error al Registrar: ",
+							"El nombre de usuario provisto ya existe");
 				}
 			}
 			FacesContext.getCurrentInstance().addMessage(null, message);
 			return retPage;
-		}catch (Exception e) {
+		} catch (Exception e) {
 			return null;
 		}
 	}
-	
-	public String delete(){
-		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Éxito al Borrar: ", "Usuario borrado exitosamente!");
+
+	public String delete() {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Éxito al Borrar: ",
+				"Usuario borrado exitosamente!");
 		String retPage = "bajaUsuarioPage";
 		try {
-			if(!tipoPerfil.ADMINISTRADOR.equals(perfilLogeado)) {
-				message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Falta de Permisos: ", "Debe ser un Usuario ADMINISTRADOR para poder acceder");
-			}else if(selectedUsuario==null){
-				message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Error al Borrar: ", "Seleccione un Usuario a borrar!");
-			}else if(!confirmarBorrado){
-				message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Error al Borrar: ", "Seleccione la casilla de confirmación!");
-			}else {
+			if (!tipoPerfil.ADMINISTRADOR.equals(perfilLogeado)) {
+				message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Falta de Permisos: ",
+						"Debe ser un Usuario ADMINISTRADOR para poder acceder");
+			} else if (selectedUsuario == null) {
+				message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Error al Borrar: ",
+						"Seleccione un Usuario a borrar!");
+			} else if (!confirmarBorrado) {
+				message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Error al Borrar: ",
+						"Seleccione la casilla de confirmación!");
+			} else {
 				usuariosEJBBean.delete(selectedUsuario.getId());
 			}
 			FacesContext.getCurrentInstance().addMessage(null, message);
 			return retPage;
-		}catch (Exception e) {
+		} catch (Exception e) {
 			return null;
-		}		
+		}
 	}
-	
-	public Usuario get(){
+
+	public Usuario get() {
 		try {
 			return usuariosEJBBean.getNA(nomAcceso);
-		}catch (Exception e) {
+		} catch (Exception e) {
 			return null;
-		}		
+		}
 	}
-	
-	public LinkedList<Usuario> getAll(){
+
+	public LinkedList<Usuario> getAll() {
 		try {
 			return usuariosEJBBean.getAll();
-		}catch (Exception e) {
+		} catch (Exception e) {
 			return null;
-		}	
+		}
 	}
-	
-	private boolean ValidarContrasena(String userTyped, String typedPwd){
+
+	private boolean ValidarContrasena(String userTyped, String typedPwd) {
 		try {
 			return usuariosEJBBean.ValidarContrasena(userTyped, typedPwd);
-		}catch (Exception e) {
+		} catch (Exception e) {
 			return false;
 		}
 	}
-	
-	
-	
+
 	/***********************************************************************************************************************************/
-	
+
 	public String login() {
-        FacesMessage message = null;
-        try {
-        	Usuario loginUser = usuariosEJBBean.getNA(nomAcceso);
-            
-            if(nomAcceso!=null && loginUser!=null && contrasena !=null && ValidarContrasena(nomAcceso, DigestUtils.md5Hex(contrasena))) {
-                message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Bienvenido", nomAcceso);
-                FacesContext.getCurrentInstance().addMessage(null, message);
-                perfilLogeado = loginUser.getTipoPerfil();
-                return "mainPage?faces-redirect=true";
-            } else {
-                message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error en Inicio de Sesión", 
-                		loginUser!=null ? "Credenciales Inválidas" : "No existe un Usuario que con coincida con los datos ingresados");
-                FacesContext.getCurrentInstance().addMessage(null, message);
-                return "index";
-            }
+		FacesMessage message = null;
+		try {
+			Usuario loginUser = usuariosEJBBean.getNA(nomAcceso);
+
+			if (nomAcceso != null && loginUser != null && contrasena != null
+					&& ValidarContrasena(nomAcceso, DigestUtils.md5Hex(contrasena))) {
+				message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Bienvenido", nomAcceso);
+				FacesContext.getCurrentInstance().addMessage(null, message);
+				perfilLogeado = loginUser.getTipoPerfil();
+				return "mainPage?faces-redirect=true";
+			} else {
+				message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error en Inicio de Sesión",
+						loginUser != null ? "Credenciales Inválidas"
+								: "No existe un Usuario que con coincida con los datos ingresados");
+				FacesContext.getCurrentInstance().addMessage(null, message);
+				return "index";
+			}
 		} catch (ServiciosException e) {
 			message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error en Inicio de Sesión", e.getMessage());
-            FacesContext.getCurrentInstance().addMessage(null, message);
-            return "index";
+			FacesContext.getCurrentInstance().addMessage(null, message);
+			return "index";
 		}
- 
-    } 
-	
+
+	}
+
 	public String cancelLogin() {
 		return "index?faces-redirect=true";
 	}
-	
 
-	
 	/**
 	 * Metodo para armar los combos
 	 */
@@ -168,50 +180,42 @@ public class UsuariosBean {
 		FacesMessage message = null;
 		try {
 			ArrayList<SelectItem> perfiles = new ArrayList<>();
-			perfiles.add(new SelectItem(PerfilUsuario.ENCARGADO, PerfilUsuario.ENCARGADO.toString()));
-			perfiles.add(new SelectItem(PerfilUsuario.PERSONAL, PerfilUsuario.PERSONAL.toString()));
-			availablePerfiles =  perfiles;
+			perfiles.add(new SelectItem(tipoPerfil.SUPERVISOR, tipoPerfil.SUPERVISOR.toString()));
+			perfiles.add(new SelectItem(tipoPerfil.OPERARIO, tipoPerfil.OPERARIO.toString()));
+			availablePerfiles = perfiles;
 		} catch (Exception e) {
 			message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error en SetUp: ", e.getMessage());
 			FacesContext.getCurrentInstance().addMessage(null, message);
 		}
 	}
-	
-	
-	public String checkRoles(){
+
+	public String checkRoles() {
 		try {
-			if(perfilLogeado==null) {
-		       return "index?faces-redirect=true";
-			}else {
+			if (perfilLogeado == null) {
+				return "index?faces-redirect=true";
+			} else {
 				return null;
 			}
-	    } catch (Exception e) {
-	        return "index?faces-redirect=true";
-	    }
+		} catch (Exception e) {
+			return "index?faces-redirect=true";
+		}
 	}
-	
+
 	public String logout() {
 		perfilLogeado = null;
 		return "index?faces-redirect=true";
 	}
-	
+
 	/***********************************************************************************************************************************/
-	
-	//Getters and Setters
-	public int getIdUsuario() {
-		return idUsuario;
+
+	// Getters and Setters
+
+	public Long getId() {
+		return id;
 	}
 
-	public void setIdUsuario(int idUsuario) {
-		this.idUsuario = idUsuario;
-	}
-
-	public String getApellido() {
-		return apellido;
-	}
-
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getNombre() {
@@ -222,28 +226,44 @@ public class UsuariosBean {
 		this.nombre = nombre;
 	}
 
-	public String getPassword() {
-		return password;
+	public String getApellido() {
+		return apellido;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
 	}
 
-	public PerfilUsuario getPerfil() {
-		return perfil;
+	public String getNomAcceso() {
+		return nomAcceso;
 	}
 
-	public void setPerfil(PerfilUsuario perfil) {
-		this.perfil = perfil;
+	public void setNomAcceso(String nomAcceso) {
+		this.nomAcceso = nomAcceso;
 	}
 
-	public String getUsuario() {
-		return usuario;
+	public String getContrasena() {
+		return contrasena;
 	}
 
-	public void setUsuario(String usuario) {
-		this.usuario = usuario;
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public tipoPerfil getTipoPerfil() {
+		return tipoPerfil;
+	}
+
+	public void setTipoPerfil(tipoPerfil tipoPerfil) {
+		this.tipoPerfil = tipoPerfil;
 	}
 
 	public List<SelectItem> getAvailablePerfiles() {
@@ -254,11 +274,11 @@ public class UsuariosBean {
 		this.availablePerfiles = availablePerfiles;
 	}
 
-	public static PerfilUsuario getPerfilLogeado() {
-		return UsuariosBean.perfilLogeado;
+	public static tipoPerfil getPerfilLogeado() {
+		return perfilLogeado;
 	}
 
-	public void setPerfilLogeado(PerfilUsuario perfilLogeado) {
+	public static void setPerfilLogeado(tipoPerfil perfilLogeado) {
 		UsuariosBean.perfilLogeado = perfilLogeado;
 	}
 
@@ -269,7 +289,7 @@ public class UsuariosBean {
 	public void setRadioFindBy(String radioFindBy) {
 		this.radioFindBy = radioFindBy;
 	}
-	
+
 	public String getFiltroBusqueda() {
 		return filtroBusqueda;
 	}
@@ -282,7 +302,7 @@ public class UsuariosBean {
 		return usuariosList;
 	}
 
-	public void setUsuariosList(List<Usuario>usuariosList) {
+	public void setUsuariosList(List<Usuario> usuariosList) {
 		this.usuariosList = usuariosList;
 	}
 
@@ -293,7 +313,7 @@ public class UsuariosBean {
 	public void setSelectedUsuario(Usuario selectedUsuario) {
 		this.selectedUsuario = selectedUsuario;
 	}
-	
+
 	public boolean isConfirmarBorrado() {
 		return confirmarBorrado;
 	}
@@ -301,6 +321,13 @@ public class UsuariosBean {
 	public void setConfirmarBorrado(boolean confirmarBorrado) {
 		this.confirmarBorrado = confirmarBorrado;
 	}
-	
-	
+
+	public UsuarioBeanRemote getUsuariosEJBBean() {
+		return usuariosEJBBean;
+	}
+
+	public void setUsuariosEJBBean(UsuarioBeanRemote usuariosEJBBean) {
+		this.usuariosEJBBean = usuariosEJBBean;
+	}
+
 }
